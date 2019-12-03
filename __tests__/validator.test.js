06eabef60389aa7b48bc48fa561f30adbc7f1eff
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-03 13:47:24 -0800

    added config files

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -3,11 +3,11 @@ const {
 } = require('../lib/validator.js');
 
 describe ('validator class module', () => {
-    testValidator = new Validator('name', {
-        type: String,
-        required: true
-      });
-    it('properly makes a validator', () => {
-        expect(testValidator.validator).toBeDefined();
-    });
+  testValidator = new Validator('name', {
+    type: String,
+    required: true
+  });
+  it('properly makes a validator', () => {
+    expect(testValidator.validator).toBeDefined();
+  });
 });
