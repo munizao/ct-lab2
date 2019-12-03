d520c9cdab82bd0d05b0c6b825364649bbc079be
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-03 13:29:41 -0800

    Added validator constructor test.

---
+++ __tests__/validator.test.js
@@ -0,0 +1,13 @@
+const {
+
+} = require('../lib/validator.js');
+
+describe ('validator class module', () => {
+    testValidator = new Validator('name', {
+        type: String,
+        required: true
+      });
+    it('properly makes a validator', () => {
+        expect(testValidator.validator).toBeDefined();
+    });
+});
