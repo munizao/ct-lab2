226daecbbcb927e9a731421e928a5a3ce126076b
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-03 13:54:04 -0800

    Added tests for fields of validator object.

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,13 +1,14 @@
 const {
-
+  Validator
 } = require('../lib/validator.js');
 
 describe ('validator class module', () => {
-  testValidator = new Validator('name', {
+  const testValidator = new Validator('name', {
     type: String,
     required: true
   });
   it('properly makes a validator', () => {
-    expect(testValidator.validator).toBeDefined();
+    expect(testValidator.type).toBeDefined();
+    expect(testValidator.required).toBeDefined();
   });
 });
