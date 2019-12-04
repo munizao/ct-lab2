186aad0b6beb4165352c4e60a7fb84ec1d3717cd
Alexandre Muniz <aom@puzzlezapper.com>
2019-12-03 16:58:16 -0800

    wrote schema constructor

--- __tests__/schema.test.js
+++ __tests__/schema.test.js
@@ -1,3 +1,27 @@
 const {
-    Schema
-  } = require('../lib/schema.js');
+  Schema
+} = require('../lib/schema.js');
+
+describe ('schema class module', () => {
+  testSchemaDefinition =
+  {
+    name: {
+      type: String,
+      required: true
+    }, 
+    age: {
+      type: Number,
+      required: true
+    },
+    weight: {
+      type: String
+    }
+  });
+
+  testSchema = new Schema(testSchemaDefinition);
+
+  it('properly makes a schema', () => {
+    expect(testSchema.validators).toBeDefined()
+  });
+
+)};
